5a693ce15c55c017022dbafd1d3bd48e2f2414eb
Paul Evans <decretist@gmail.com>
2024-02-26 10:12:16 -0800

    Add regex practice.py

---
+++ practice.py
@@ -0,0 +1,8 @@
+# Jake Bayon (jbayon@sandiego.edu)
+# 26 February 2024
+
+def main():
+    text = 'Humanum genus duobus regitur, naturali uidelicet iure et moribus. Ius naturae est, quod in lege et euangelio continetur, quo quisque iubetur alii facere, quod sibi uult fieri, et prohibetur alii inferre, quod sibi nolit fieri. Unde Christus in euangelio: "Omnia quecunque uultis ut faciant uobis homines, et uos eadem facite illis. Haec est enim lex et prophetae." Hinc Ysidorus in V. libro Ethimologiarum ait:'
+
+if __name__ == "__main__":
+    main()
